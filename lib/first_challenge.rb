b305a465bb6218086c705758c4dfdf3598cd94db
a lan <bluenowte97@gmail.com>
2018-11-08 21:37:44 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,23 @@ def first_challenge
     }
   }
 
-  #your code here
+  # first method, call loops at each level until get to array
+  #then use delete_if on array
+  contacts.each do |person, info|
 
-
+    info.each do |person_field, value|
+      if person_field == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor = "strawberry"
+        end
+      end
+    end
+  end
+   #second method , just go directly to the array, then use delete_if
+   icecream = contacts["Freddy Mercury"][:favorite_icecream_flavors]
+    icecream.delete_if do |flavor|
+      flavor = "strawberry"
+    end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
